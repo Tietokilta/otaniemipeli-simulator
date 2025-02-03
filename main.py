ccc0e224857ee5d7ea9c2c37552214b3d473b55d
Kiltis <you@example.com>
2025-02-03 03:22:01 +0200

    fix mp

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from random import randint
 from queue import Empty
-from multiprocessing import Pipe, Process
-from multiprocessing.connection import Connection
+from concurrent.futures import ProcessPoolExecutor, Future
 import sys
 
 from matplotlib import pyplot as p
@@ -23,7 +22,10 @@ nvisits = [[0 for _ in squares] for _ in nteams_opts]
 nsqdrinks = [[0 for _ in squares] for _ in nteams_opts]
 nsqportions = [[0 for _ in squares] for _ in nteams_opts]
 
-def simulate(i: int, pipe: Connection | None):
+bsize = 128
+bmask = bsize - 1
+
+def simulate(i: int, ngames: int):
     ndrinksavg = []
     nportionsavg = []
     ndrinkstop = []
@@ -36,12 +38,12 @@ def simulate(i: int, pipe: Connection | None):
 
     nteams = nteams_opts[i]
     for j in range(ngames):
-        res = game(nteams)
-        if j & 127 == 127:
-            if pipe:
-                pipe.send(128)
-            else:
-                print(f"\r{nteams} teams {j} games     ", end="", flush=True)
+        try:
+            res = game(nteams)
+        except KeyboardInterrupt:
+            break
+        if ngames > bsize and j & bmask == bmask:
+            print(f"\r{nteams} teams {j} games     ", end="", flush=True)
         ndrinksavg.append(res.avg_drinks)
         ndrinkstop.append(res.top_drinks)
         nportionsavg.append(res.avg_portions)
@@ -68,48 +70,44 @@ def simulate(i: int, pipe: Connection | None):
         nsqdrinks,
         nsqportions,
     )
-    if pipe:
-        pipe.send(res)
     return res
 
 if __name__ == "__main__":
     if sys.argv[3] == "m":
-        procs: list[Process] = []
-        recv, send = Pipe(False)
-        for i in range(len(nteams_opts)):
-            proc = Process(target=simulate, args=(i, send))
-            procs.append(proc)
-            proc.start()
-        total = 0
-        while alive := sum(proc.is_alive() for proc in procs):
-            print(f"\r{alive} threads {total} games", end="", flush=True)
-            while recv.poll(0.1):
-                item = recv.recv()
-                if isinstance(item, int):
-                    total += item
-                else:
-                    ndrinksavg[item[0]] = item[1]
-                    nportionsavg[item[0]] = item[2]
-                    ndrinkstop[item[0]] = item[3]
-                    nportionstop[item[0]] = item[4]
-                    nrounds[item[0]] = item[5]
-                    nturns[item[0]] = item[6]
-                    nvisits[item[0]] = item[7]
-                    nsqdrinks[item[0]] = item[8]
-                    nsqportions[item[0]] = item[9]
-            print()
+        pool = ProcessPoolExecutor(len(nteams_opts))
+        try:
+            futs: list[Future] = []
+            for i in range(len(nteams_opts)):
+                for j in range(round(ngames / bsize)):
+                    futs.append(pool.submit(simulate, i, bsize))
+            total = 0
+            for fut in futs:
+                total += bsize
+                res = fut.result()
+                print(f"\rpool: {nteams_opts[res[0]]} teams {total} games", end="", flush=True)
+                ndrinksavg[res[0]] += res[1]
+                nportionsavg[res[0]] += res[2]
+                ndrinkstop[res[0]] += res[3]
+                nportionstop[res[0]] += res[4]
+                nrounds[res[0]] += res[5]
+                nturns[res[0]] += res[6]
+                nvisits[res[0]] += res[7]
+                nsqdrinks[res[0]] += res[8]
+                nsqportions[res[0]] += res[9]
+        finally:
+            pool.shutdown(True, cancel_futures=True)
     elif sys.argv[3] == "s":
         for i in range(len(nteams_opts)):
-            item = simulate(i, None)
-            ndrinksavg[item[0]] = item[1]
-            nportionsavg[item[0]] = item[2]
-            ndrinkstop[item[0]] = item[3]
-            nportionstop[item[0]] = item[4]
-            nrounds[item[0]] = item[5]
-            nturns[item[0]] = item[6]
-            nvisits[item[0]] = item[7]
-            nsqdrinks[item[0]] = item[8]
-            nsqportions[item[0]] = item[9]
+            res = simulate(i, None)
+            ndrinksavg[res[0]] = res[1]
+            nportionsavg[res[0]] = res[2]
+            ndrinkstop[res[0]] = res[3]
+            nportionstop[res[0]] = res[4]
+            nrounds[res[0]] = res[5]
+            nturns[res[0]] = res[6]
+            nvisits[res[0]] = res[7]
+            nsqdrinks[res[0]] = res[8]
+            nsqportions[res[0]] = res[9]
 
     print()
 
